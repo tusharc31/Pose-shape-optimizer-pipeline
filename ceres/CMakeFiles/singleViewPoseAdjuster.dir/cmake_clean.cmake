ed111edf6d40f07f0c78770a80fd80b6cd3b132c
Tushar Choudhary <hitusharonline31@gmail.com>
2021-08-10 11:18:13 +0530

    Minor changes

--- ceres/CMakeFiles/singleViewPoseAdjuster.dir/cmake_clean.cmake
+++ ceres/CMakeFiles/singleViewPoseAdjuster.dir/cmake_clean.cmake
@@ -1,7 +1,7 @@
 file(REMOVE_RECURSE
   "CMakeFiles/singleViewPoseAdjuster.dir/singleViewPoseAdjuster.cc.o"
-  "singleViewPoseAdjuster.pdb"
   "singleViewPoseAdjuster"
+  "singleViewPoseAdjuster.pdb"
 )
 
 # Per-language clean rules from dependency scanning.
